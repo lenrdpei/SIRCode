6e09d68a38d190aed88d8b9f52a0e84553d8b35c
lenrdpei <92371430+lenrdpei@users.noreply.github.com>
2025-04-27 17:10:43 +0800

    docstring for betagen

--- my_code/BetaGen.jl
+++ my_code/BetaGen.jl
@@ -4,6 +4,22 @@ using Random, LinearAlgebra, Distributions
 
 export generate_B, generate_beta
 
+"""
+    generate_B(p::Int; use_continuous_B::Bool = false) -> Vector{Float64}
+
+Generates a random vector B of length p, where each element is either 0 or 1.
+The vector can be optionally scaled by a random vector of length p.
+
+# Arguments
+- `p::Int`: The length of the vector B.
+- `use_continuous_B::Bool`: If true, the elements of B are scaled by a random vector of length p.
+    
+    Default is false.
+
+# Returns
+- `Vector{Float64}`: A vector of length p, where each element is either 0 or 1.
+    If `use_continuous_B` is true, the elements are scaled by a random vector of length p.
+"""
 function generate_B(
     p::Int;           # feature dimension
     use_continuous_B::Bool = false
@@ -17,6 +33,27 @@ function generate_B(
     return B
 end
 
+"""
+    generate_beta(pattern_B::Vector, num_nodes::Int; p::Int = 10, deg::Int = 2, σ::Float64 = 0.1) -> Tuple{Matrix{Float64}, Vector{Float64}}
+
+Generates a random vector β of length |V|, where |V| is the number of nodes in the graph.
+The vector is generated based on a design matrix Z and a random vector B.
+The generation process includes a non-linear transformation and additive Gaussian noise.
+
+
+# Arguments
+- `pattern_B::Vector`: The underlying pattern (matrix B) used for generating β.
+- `num_nodes::Int`: The number of nodes in the graph (|V|).
+- `p::Int`: The feature dimension (default is 10).
+- `deg::Int`: The degree of non-linearity (default is 2).
+- `σ::Float64`: The standard deviation of the additive Gaussian noise (default is 0.1).
+
+
+# Returns
+- `Z::Matrix{Float64}`: The design matrix of shape (|V|, p).
+- `β::Vector{Float64}`: The generated vector of shape (|V|,).
+    The elements of β are in the range [0, 1].
+"""
 function generate_beta(
     pattern_B::Vector, # underlying pattern (matrix B)
     num_nodes::Int;    # |V|
@@ -50,7 +87,7 @@ function generate_beta(
 end
 
 # Example usage
-B = generate_B(10; use_continuous_B=true)
-Z, β = generate_beta(B, 100; p=10, deg=2, σ=0.1)
+# B = generate_B(10; use_continuous_B=true)
+# Z, β = generate_beta(B, 100; p=10, deg=2, σ=0.1)
 
 end # module BetaGen
